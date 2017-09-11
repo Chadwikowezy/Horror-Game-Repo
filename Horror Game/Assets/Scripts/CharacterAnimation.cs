9bd8e7d2a9022fb9c6fc3aa4ae10b97435c8d8e8
Chadwikowezy <chad_mcleavy@hotmail.com>
2017-09-10 23:42:51 -0700

    - Put in left arm animations

---
+++ Horror Game/Assets/Scripts/CharacterAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    public Animator leftArm;
+    public Animator rightArm;
+
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        print(_rb.velocity.magnitude);
+        AnimateLeftArm();
+    }
+
+    void AnimateLeftArm()
+    {
+        leftArm.SetFloat("Rigidbody Velocity", _rb.velocity.magnitude);
+    }
+}
