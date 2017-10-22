e4d64c33d8a0e9f0c32c5d7d9ead771fa9f5d2cf
Hopesresolve <gonzales.phil@hotmail.com>
2017-10-22 12:52:30 -0700

    Added Prototype camera button that allows visibility of numbers for safe puzzle, added in chad's Phone ui to enable and disable on camera equip activation

---
+++ Horror Game/Assets/PhoneManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneManager : MonoBehaviour
+{
+    private Camera cam;
+    public bool isOn = false;
+    public GameObject[] numbers;
+    public GameObject topBanner;
+    public GameObject bottomBanner;
+
+    void Start ()
+    {
+        cam = Camera.main;
+    }
+
+    public void LookThruPhoneLens()
+    {
+        numbers = GameObject.FindGameObjectsWithTag("Number");
+        if (isOn == false)
+        {
+            StartCoroutine(LookThruPhoneLensDelay());
+        }
+        else if(isOn == true)
+        {
+            StartCoroutine(DisablePhoneLensDelay());
+        }
+    }
+    IEnumerator LookThruPhoneLensDelay()
+    {
+        yield return new WaitForSeconds(1.25f);
+        foreach(GameObject number in numbers)
+        {
+            number.layer = LayerMask.NameToLayer("Default");
+            topBanner.SetActive(true);
+            bottomBanner.SetActive(true);
+            isOn = true;
+        }
+    }
+    IEnumerator DisablePhoneLensDelay()
+    {
+        yield return new WaitForSeconds(.75f);
+        foreach (GameObject number in numbers)
+        {
+            number.layer = LayerMask.NameToLayer("Number");
+            topBanner.SetActive(false);
+            bottomBanner.SetActive(false);
+            isOn = false;
+        }
+    }
+}
