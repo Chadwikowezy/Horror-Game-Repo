ba034a120a84a369b859218b149cfd0b36758f51
Hopesresolve <gonzales.phil@hotmail.com>
2017-10-26 16:55:06 -0700

    Added and set up phone, UI and functionality of phone is good to go, just gotta add charging stations.

--- Horror Game/Assets/PhoneManager.cs
+++ Horror Game/Assets/PhoneManager.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PhoneManager : MonoBehaviour
 {
     public bool isOn = false;
+    public bool phoneIsDead = false;
     public GameObject phoneCamera;
+
+    private float maxBatteryLife = 60,
+        minBatteryLife = 0, 
+        batteryDrainRate = 1;
+    public float currentBatteryLife;
+    public Image phoneBattery;
+    public GameObject phoneDeadNotification;
 
     public void LookThruPhoneLens()
     {
-        if (isOn == false)
+        if (isOn == false && phoneIsDead == false)
         {
             StartCoroutine(LookThruPhoneLensDelay());
         }
@@ -17,12 +26,60 @@ public class PhoneManager : MonoBehaviour
         {
             StartCoroutine(DisablePhoneLensDelay());
         }
+        else if(phoneIsDead == true)
+        {
+            StartCoroutine(phoneDeadNotificationDelay());
+        }
+    }
+
+    IEnumerator phoneDeadNotificationDelay()
+    {
+        phoneDeadNotification.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        phoneDeadNotification.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (isOn == true)
+        {
+            if (currentBatteryLife <= maxBatteryLife && currentBatteryLife >= minBatteryLife)
+            {
+                currentBatteryLife -= Time.deltaTime * batteryDrainRate;
+            }
+            if (currentBatteryLife > maxBatteryLife)
+            {
+                currentBatteryLife = maxBatteryLife;
+            }
+            if (currentBatteryLife < minBatteryLife)
+            {
+                StartCoroutine(DisablePhoneLensDelay());
+                phoneIsDead = true;                
+                currentBatteryLife = minBatteryLife;
+                phoneDeadNotificationDelay();
+            }
+
+            phoneBattery.fillAmount = currentBatteryLife / maxBatteryLife;
+
+            if(phoneBattery.fillAmount < 1f && phoneBattery.fillAmount > .75f)
+            {
+                phoneBattery.color = Color.green;
+            }
+            if(phoneBattery.fillAmount <= .66f)
+            {
+                phoneBattery.color = Color.Lerp(Color.green, Color.yellow, 1f);
+            }
+            if(phoneBattery.fillAmount <= .33f)
+            {
+                phoneBattery.color = Color.Lerp(Color.yellow, Color.red, 1f);
+            }
+        }
     }
     IEnumerator LookThruPhoneLensDelay()
     {
         yield return new WaitForSeconds(1.25f);
         phoneCamera.SetActive(true);
-        isOn = true;        
+        isOn = true;       
     }
     IEnumerator DisablePhoneLensDelay()
     {
