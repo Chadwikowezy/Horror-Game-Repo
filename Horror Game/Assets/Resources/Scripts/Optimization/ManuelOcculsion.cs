1058737203c326574ad52cfb40441c0c096a8a9c
Hopesresolve <gonzales.phil@hotmail.com>
2017-11-05 17:44:01 -0800

    Added in manuel occlusion mapping script, works great, just havent integrated it yet cause waiting for mansion to have all art assets first. set directional light to have no presence in the lighting settings, utilzing the fog system in lighting to have that great pitdh black but shitouette lighting effect, idk howe i'm gonna do point lights spread throughout mansion quite yet though.

---
+++ Horror Game/Assets/Resources/Scripts/Optimization/ManuelOcculsion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManuelOcculsion : MonoBehaviour
+{
+    private Camera cam;
+    private GameObject[] occlusionObjects;
+
+	void Start ()
+    {
+        cam = GetComponent<Camera>();
+        occlusionObjects = GameObject.FindGameObjectsWithTag("Occlusion");
+    }
+	
+	void Update ()
+    {
+        OcclusionCulling();
+    }
+
+    void OcclusionCulling()
+    {
+        foreach(GameObject occlusionObj in occlusionObjects)
+        {
+            Vector3 screenPoint = cam.WorldToViewportPoint(occlusionObj.transform.position);
+            bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && 
+                screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+
+            if(!onScreen)
+            {
+                occlusionObj.SetActive(false);
+            }
+            else
+            {
+                occlusionObj.SetActive(true);
+            }
+        }
+    }
+}
