1272f4261988cb80d1dcb7e78bf3db07a12d8f0b
Chad McLeavy <chad_mcleavy@hotmail.com>
2017-11-15 16:52:38 -0800

    Started code for monster AI.

---
+++ Horror Game/Assets/Resources/Scripts/Monsters/MonsterStates.cs
@@ -0,0 +1,7 @@
+﻿public enum MonsterStates
+{
+    Idle,
+    Patrol,
+    Alerted,
+    Chasing
+}
---
+++ Horror Game/Assets/Resources/Scripts/Monsters/Spector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spector : MonoBehaviour
+{
+    //Variables
+    private MonsterStates _currentState;
+
+    //Properties
+    public MonsterStates CurrentState
+    {
+        get { return _currentState; }
+        set { _currentState = value; }
+    }
+
+    //Functions
+
+    void wander()
+    { }
+}
