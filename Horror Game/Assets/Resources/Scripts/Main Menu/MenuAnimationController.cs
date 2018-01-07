92cc93338ec3526764d99ba79f725b1707e8d099
Chadwikowezy <chad_mcleavy@hotmail.com>
2018-01-07 01:54:50 -0800

    - Layed out main menu

---
+++ Horror Game/Assets/Resources/Scripts/Main Menu/MenuAnimationController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAnimationController : MonoBehaviour
+{
+    public Animator monsterAnimator;
+    public AnimationClip monsterAnimation;
+
+    private float timer;
+    private float monsterAnimLength;
+
+    private void Start()
+    {
+        timer = 0;
+        monsterAnimLength = monsterAnimation.length - 0.5f;
+    }
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > monsterAnimLength)
+        {
+            monsterAnimator.CrossFade(monsterAnimation.name, 0.5f);
+            timer = 0;
+            print("worked");
+        }
+    }
+}
