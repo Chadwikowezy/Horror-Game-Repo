b0165be6afd648061945c50cf2ead21f3d02bb5d
Chadwikowezy <chad_mcleavy@hotmail.com>
2018-01-07 15:41:11 -0800

    - Imported my particles package - Added new particles to main menu - Added flickering light effect to main menu - Finished main menu environ ment and layout (No functionallity yet)

---
+++ Horror Game/Assets/Resources/Scripts/Main Menu/MenuController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    [Header("Flickering Light")]
+    public Light flickeringLight;
+    public int minNumOfFlickers;
+    public int maxNumOfFlickers;
+    public float minWaitTime;
+    public float maxWaitTime;
+
+    private void Start()
+    {
+        StartCoroutine(FlickerLight());
+    }
+
+    IEnumerator FlickerLight()
+    {
+        int numOfFlickers;
+
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+
+            numOfFlickers = Random.Range(minNumOfFlickers, maxNumOfFlickers);
+
+            for (int i = 0; i < numOfFlickers; i++)
+            {
+                yield return new WaitForSeconds(Random.Range(0.05f, 0.2f));
+
+                flickeringLight.enabled = !flickeringLight.enabled;
+            }
+        }
+    }
+}
