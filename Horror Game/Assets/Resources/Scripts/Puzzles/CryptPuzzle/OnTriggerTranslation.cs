9171eaa35e479042bb6e7087cb84c33654ee9540
Steven Moreno <stevenxmoreno@gmail.com>
2018-01-21 16:54:04 -0800

    -Created crypt prefabs -OnTriggerTranslation.cs -Added new metal texture and material

---
+++ Horror Game/Assets/Resources/Scripts/Puzzles/CryptPuzzle/OnTriggerTranslation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerTranslation : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject triggerPrefab;
+    [SerializeField]
+    private GameObject targetPrefab;
+
+    [SerializeField]
+    private float xPos;
+    [SerializeField]
+    private float yPos;
+    [SerializeField]
+    private float zPos;
+
+    [SerializeField]
+    private bool activated = false;
+
+    private void TranslateBlock()
+    {
+        targetPrefab.transform.Translate(xPos, yPos, zPos);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            
+            if (activated == false)
+            {
+                TranslateBlock();
+                activated = true;
+            }
+            
+            print("Collision");
+        };
+    }
+}
