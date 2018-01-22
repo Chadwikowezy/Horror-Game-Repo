17e6dbdf5b4f719c445393b2dd7b7b9eb1fbfd8c
Hopesresolve <gonzales.phil@hotmail.com>
2018-01-21 18:30:03 -0800

    Switched all world space canvas to just a modeled out version for better performance. changed the main canvas with main UI into multiple, as it's cheaper to have multiple canvases over one with everything. didn't appear to be any console errors when switching both these when running thru level.

---
+++ Horror Game/Assets/ActivateLoadScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateLoadScreen : MonoBehaviour
+{
+    public GameObject loadEffect;
+
+	void Awake ()
+    {
+        StartCoroutine(OnStartLoadDelay());
+	}
+	
+	IEnumerator OnStartLoadDelay()
+    {
+        loadEffect.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        loadEffect.SetActive(false);
+    }
+}
