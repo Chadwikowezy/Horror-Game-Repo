3e9170d720c4fc2e4e76ad433ba75298bd8682af
Chadwikowezy <chad_mcleavy@hotmail.com>
2018-01-23 15:32:21 -0800

    - Began work on intro cutscene for graveyard - Created cutscene player prefab (has idle and walk anims)

---
+++ Horror Game/Assets/Resources/Scripts/CutscenePlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CutscenePlayer : MonoBehaviour
+{
+    public Transform[] waypoints;
+
+    private NavMeshAgent _myAgent;
+    private Animator _anim;
+
+    private void Start()
+    {
+        _myAgent = GetComponent<NavMeshAgent>();
+        _myAgent.SetDestination(waypoints[0].position);
+        _anim = GetComponentInChildren<Animator>();
+    }
+    private void Update()
+    {
+        _anim.SetFloat("Speed", _myAgent.velocity.magnitude);
+    }
+}
