c7dac51d3a48a4691f62f8c9ff5f0f2f41d97af6
Chadwikowezy <chad_mcleavy@hotmail.com>
2018-01-24 13:54:06 -0800

    - Started second half of intro animation - Created gravestone script with suedo code - Created collect button  that activates when player reaches gravestone (no functionality yet)

---
+++ Horror Game/Assets/Resources/Scripts/Cutscene/Graveyard_Gravestone.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graveyard_Gravestone : MonoBehaviour
+{
+    public Graveyard_CutsceneManager manager;
+    public float detectionDistance;
+
+    public GameObject collectButton;
+
+    private void Start()
+    {
+        manager = FindObjectOfType<Graveyard_CutsceneManager>();
+    }
+    private void Update()
+    {
+        if ((manager.player.transform.position - transform.position).magnitude < detectionDistance)
+            showCollectButton();
+    }
+
+    void showCollectButton()
+    {
+        //Play locket intro animation that transitions into examining animation
+        //Set player "Stoped" property to true
+
+        collectButton.SetActive(true);
+    }
+    public void collectObject()
+    {
+        //Play locket collect animation
+
+        manager.monster.SetActive(true);
+        //monster plays jump scare animation on activation
+        //player plays jump back animation on monster activation
+        //Set player "Stoped" property to false
+    }
+}
