7d978edf4edb0b1708f5b00a5f185bdd0b9aa7a8
Steven Moreno <stevenxmoreno@gmail.com>
2018-01-24 16:55:31 -0800

    -OnTriggerText.cs -OnTriggerLerp.cs -Imported EnglishTowne Font Asset -Began logic for mausoleum puzzle

---
+++ Horror Game/Assets/Resources/Scripts/Puzzles/MazePuzzle/OnTriggerLerp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerLerp : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject triggerPrefab;
+    [SerializeField]
+    private GameObject targetPrefab;
+
+    [SerializeField]
+    private float torque;
+
+    private void LerpRotation()
+    {
+        Quaternion rot = transform.rotation;
+        Quaternion newRot = Quaternion.Euler(0, 0, 45);
+        transform.rotation = Quaternion.RotateTowards(rot, newRot, Time.deltaTime * torque);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            LerpRotation();
+            print("Collision");
+        };
+    }
+}
