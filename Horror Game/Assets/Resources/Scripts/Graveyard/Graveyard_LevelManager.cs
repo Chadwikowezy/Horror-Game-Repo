3328940a0769a28cef9a9c659a84d1d8234bc530
Chadwikowezy <chad_mcleavy@hotmail.com>
2018-01-29 15:41:25 -0800

    - Created light glow effect for mausoleum lanters

--- Horror Game/Assets/Resources/Scripts/Graveyard/Graveyard_LevelManager.cs
+++ Horror Game/Assets/Resources/Scripts/Graveyard/Graveyard_LevelManager.cs
@@ -33,9 +33,10 @@ public class Graveyard_LevelManager : MonoBehaviour
                 startingLights[i].color = _currentLightColor;
         }
 
-        yield return new WaitForSeconds(3f);
-
         for (int i = 0; i < startingLights.Length; i++)
+        {
             startingLights[i].gameObject.GetComponent<LightGlow>().enabled = true;
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
--- Horror Game/Assets/Resources/Scripts/Graveyard/LightGlow.cs
+++ Horror Game/Assets/Resources/Scripts/Graveyard/LightGlow.cs
@@ -6,19 +6,36 @@ using UnityEngine;
 
 public class LightGlow : MonoBehaviour
 {
-    public float startIntensity;
-    public float amplitude;
-    public float period;
+    public bool useDefaultValuesInstead = true;
 
+    public float startIntensity;
+    public float maxIntensity;
+    public float intensityChange;
+
+    private bool _addingIntensity;
     private Light _light;
 
     private void Start()
     {
         _light = GetComponent<Light>();
         _light.intensity = startIntensity;
+        _addingIntensity = true;
+
+        if (useDefaultValuesInstead)
+        {
+            startIntensity = _light.intensity;
+            maxIntensity = startIntensity + 1;
+            intensityChange = 0.01f;
+        }
     }
-    private void Update()
+    private void FixedUpdate()
     {
-        
+        if (_light.intensity < startIntensity || _light.intensity > maxIntensity)
+            _addingIntensity = !_addingIntensity;
+
+        if (_addingIntensity)
+            _light.intensity += intensityChange;
+        else
+            _light.intensity -= intensityChange;
     }
 }
