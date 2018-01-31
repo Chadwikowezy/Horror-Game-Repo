61dc8a8f8557b5e1cefc603c971d655906e83947
Steven Moreno <stevenxmoreno@gmail.com>
2018-01-30 19:28:20 -0800

    Tidiness push

--- Horror Game/Assets/Resources/Scripts/Puzzles/Maze&Crypt/MazePuzzle/Rotation.cs
+++ Horror Game/Assets/Resources/Scripts/Puzzles/Maze&Crypt/MazePuzzle/Rotation.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
+    #region Variables
     [SerializeField]
     private GameObject triggerPrefab;
 
@@ -11,14 +12,68 @@ public class Rotation : MonoBehaviour
     public float speed;
 
     public float minAngle = 0.0F;
-    public float maxAngle = 45.0F;
+    public float maxAngle = 90.0F;
+    public float value;
+
+    public Vector3 rot,
+                newRot;
+    #endregion
+
+    #region Coroutines
+    IEnumerator MoveObject(Vector3 target, float overTime)
+    {
+        float startTime = Time.time;
+        Quaternion targetRot = Quaternion.Euler(target);
+        Quaternion _newRot;
+        while (transform.localRotation.eulerAngles.y < target.z - 1)
+        {
+            _newRot = Quaternion.Slerp(transform.rotation, targetRot, speed);
+            gameObject.transform.rotation = _newRot;
+            yield return new WaitForFixedUpdate();
+        }
+
+        newRot.z += value;
+       //fix 
+    }
+
+    IEnumerator MoveObject_02(Vector3 target, float overTime)
+    {
+        float startTime = Time.time;
+        Quaternion targetRot = Quaternion.Euler(target);
+        Quaternion _newRot;
+        while (transform.localRotation.eulerAngles.y < target.y - 1)
+        {
+            _newRot = Quaternion.Slerp(transform.rotation, targetRot, speed);
+            gameObject.transform.rotation = _newRot;
+            yield return new WaitForFixedUpdate();
+        }
+        newRot.y += value;
+    }
+    #endregion
+
+    #region Functions
+    public void Lerp()
+    {
+        rot = gameObject.transform.eulerAngles;
+        StartCoroutine(MoveObject(newRot, (Time.time - 0) / 5));
+    }
+
+    public void Lerp_02()
+    {
+        rot = gameObject.transform.eulerAngles;
+        StartCoroutine(MoveObject_02(newRot, (Time.time - 0) / 5));
+    }
+
+    public void PlayAudio()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+    }
 
     public void Rotate()
     {
         float angle = Mathf.LerpAngle(minAngle, maxAngle, speed);
-        transform.eulerAngles = new Vector3(0, 0, angle);
-        //triggerPrefab.transform.rotation = Quaternion.Slerp(triggerPrefab.transform.rotation, Quaternion.Euler(0, 0, 45), speed );
-        print("rotated");
+        transform.eulerAngles = transform.eulerAngles + new Vector3(0, 0, angle);
     }
-	
+	#endregion
 }
