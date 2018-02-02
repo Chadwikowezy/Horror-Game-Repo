34a227b9e73c7add3f7952e4ba25d3150d4f759f
Chadwikowezy <chad_mcleavy@hotmail.com>
2018-02-01 20:29:43 -0800

    - Added in matches mechanic
    Works fully, might want to polish a little later down the line.

---
+++ Horror Game/Assets/Resources/Scripts/Graveyard/Match.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Match : MonoBehaviour
+{
+    public Transform matchTargetPosition;
+
+    public float matchLiftime;
+    public float matchSpeed;
+
+    private float currentMatchLife;
+
+    private void Update()
+    {
+        currentMatchLife += Time.deltaTime;
+
+        if (currentMatchLife > matchLiftime)
+            Destroy(gameObject);
+    }
+    private void FixedUpdate()
+    {
+        if (matchTargetPosition != null)
+            if ((transform.position - matchTargetPosition.position).magnitude > 0.01f)
+                transform.position = Vector3.Lerp(transform.position, matchTargetPosition.position, matchSpeed);
+    }
+
+    public void strikeMatch(Transform _matchTargetPosition)
+    {
+        matchTargetPosition = _matchTargetPosition;
+    }
+}
---
+++ Horror Game/Assets/Resources/Scripts/Graveyard/MatchManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatchManager : MonoBehaviour
+{
+    public GameObject matchPrefab;
+    public GameObject targetParentObject;
+
+    public int currentMatchCount;
+
+    public float noMatchesFadeSpeed;
+
+    public Image noMatchesImage;
+
+    private GameObject _currentMatch;
+
+    private void FixedUpdate()
+    {
+        if (noMatchesImage.color != Color.clear)
+            noMatchesImage.color = Color.Lerp(noMatchesImage.color, Color.clear, noMatchesFadeSpeed);
+    }
+
+    public void strikeMatch()
+    {
+        if (_currentMatch != null)
+            return;
+
+        if (currentMatchCount > 0)
+        {
+            currentMatchCount--;
+            _currentMatch = Instantiate(matchPrefab, targetParentObject.transform);
+        }
+        else
+        {
+            noMatchesImage.color = Color.white;
+        }
+    }
+    public void strikeMatch(Transform targetPosition)
+    {
+        if (_currentMatch != null)
+            return;
+
+        if (currentMatchCount > 0)
+        {
+            currentMatchCount--;
+            _currentMatch = Instantiate(matchPrefab, targetPosition.position, Quaternion.identity);
+
+            _currentMatch.GetComponent<Match>().strikeMatch(targetPosition);
+        }
+        else
+        {
+            noMatchesImage.color = Color.white;
+        }
+    }
+}
