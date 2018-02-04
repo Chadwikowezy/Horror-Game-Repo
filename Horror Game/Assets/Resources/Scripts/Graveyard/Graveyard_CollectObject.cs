3f784e94657389a06751da0cec069efcec0e51a5
Chadwikowezy <chad_mcleavy@hotmail.com>
2018-02-03 22:32:01 -0800

    - began coding graveyard puzzle items

---
+++ Horror Game/Assets/Resources/Scripts/Graveyard/Graveyard_CollectObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graveyard_CollectObject : MonoBehaviour
+{
+    public bool compass;
+    public bool knife;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (compass)
+            {
+                //activate compass collect button
+            }
+            else
+            {
+                //activate knife collect button
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (compass)
+            {
+                //de-activate compass collect button
+            }
+            else
+            {
+                //de-activate knife collect button
+            }
+        }
+    }
+}
