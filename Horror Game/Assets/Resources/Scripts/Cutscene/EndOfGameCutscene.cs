a2f0bb2e860993148290b664202aa18a0b655692
Chadwikowezy <chad_mcleavy@hotmail.com>
2018-02-05 17:09:05 -0800

    - Began work on end of game cutscene

---
+++ Horror Game/Assets/Resources/Scripts/Cutscene/EndOfGameCutscene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndOfGameCutscene : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject spector;
+    public GameObject cam;
+
+    public float rotateSpeed;
+
+    [Space(10), Header("Look Targets")]
+    public GameObject[] targets;
+
+    [Space(10), Header("Wait Times")]
+    public float[] waitTimes;
+
+    private void Start()
+    {
+        StartCoroutine(startCutscene());
+    }
+
+    IEnumerator startCutscene()
+    {
+        for (int i = 0; i < targets.Length; i++)
+        {
+            cam.GetComponent<PlayerCamera>().lookTarget = targets[i].transform;
+
+            yield return new WaitForSeconds(waitTimes[i]);
+        }
+    }
+}
---
+++ Horror Game/Assets/Resources/Scripts/Cutscene/PlayerCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform positionTarget;
+    public Transform lookTarget;
+
+    public float moveSpeed;
+    public float rotateSpeed;
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, positionTarget.position, moveSpeed);
+        rotateCam();
+    }
+
+    void rotateCam()
+    {
+        Vector3 _lookTarget = (lookTarget.position - transform.position).normalized;
+        Quaternion newLookRotation = Quaternion.LookRotation(_lookTarget);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, newLookRotation, rotateSpeed);
+    }
+}
